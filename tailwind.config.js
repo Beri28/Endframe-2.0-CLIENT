9b700286c717b5807b3ebb2c0fe7b1b588fe6226
KingoDreamsCode <kingokingsleykaah@gmail>
2024-11-11 12:31:30 +0100

    Screens and Pages

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        indigo: {
+          600: '#5D5DFF',
+        }
+      },
+      spacing: {
+        '128': '32rem',
+      },
+    }
+  },
+  plugins: [],
+}
